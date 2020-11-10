a812270e99873a4dad3a637cfe7103aea27405bc
Heeyeon Lee <heeyoenl@byu.edu>
2020-11-10 06:40:42 -0700

    added merge sort

---
+++ data_structures/sort/merge_sort.py
@@ -0,0 +1,34 @@
+# merge sort uses divide-and-conquer algorithm, meaning it will break down to smaller problems and solve it.
+# Since it divdies in half and merge makes it double of its size, its time complexity is O(nlogn). -> we are comparing each one that is n work time.
+# It is also recursive.
+# merge sort is good for big data because you can save half of the data when you merge sort.
+import sys
+
+def merge_sort(arr):
+    merge_sort_2(arr, 0, len(arr)-1) # user calls given one array
+
+def merge_sort_2(arr, first, last):
+    if first < last:    # if arr is longer than one
+        mid = (first + last) // 2     # find the mid index
+        merge_sort_2(arr, first, mid)    # left side
+        merge_sort_2(arr, mid+1, last)   # right side
+        merge(arr, first, mid, last)     # merge
+
+def merge(arr, first, mid, last):
+    L = arr[first:mid+1]    # copy the first half of the list
+    R = arr[mid+1:last+1]   # copy the last half of the list
+    L.append(sys.maxsize)
+    R.append(sys.maxsize)
+    i = j = 0
+    for k in range(first, last+1):
+        if L[i] <= R[j]:    # if left first element is smaller than the right first element
+            arr[k] = L[i]   # then add left[i] to array
+            i += 1          # move pointer to the next element in left
+        else:
+            arr[k] = R[j]
+            j += 1
+
+A = [5,9,1,2,4,8,6,3,7]
+print(A)
+merge_sort(A)
+print(A)
