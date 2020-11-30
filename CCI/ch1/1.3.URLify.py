d4581244f6eb31b4bc98d472915547f4def5aa9d
Heeyeon Lee <heeyoenl@byu.edu>
2020-11-30 12:46:27 -0700

    first commit CCI 1.3

---
+++ CCI/ch1/1.3.URLify.py
@@ -0,0 +1,10 @@
+# 1.3 URLify
+# Write a method to replace all spaces in a string with '%20'.
+# You may assume that the string has sufficient space at the end to hold the additional characters, 
+# and that you are given the "true" length of the string.
+# (Note: If implementing in Java, please use a charactere array so that you can perform this operation in place.)
+# Example:
+# input: "Mr John Smith     ", 13
+# output: "Mr%20John$20Smith"
+# Hints: #53, #118
+
