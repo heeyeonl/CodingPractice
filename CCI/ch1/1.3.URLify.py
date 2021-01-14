bf44ea6e2e83449da7531f055f0be8c99cb7f650
Heeyeon Lee <heeyoenl@byu.edu>
2021-01-14 12:21:16 -0700

    working on CCI1.3

--- CCI/ch1/1.3.URLify.py
+++ CCI/ch1/1.3.URLify.py
@@ -2,9 +2,31 @@
 # Write a method to replace all spaces in a string with '%20'.
 # You may assume that the string has sufficient space at the end to hold the additional characters, 
 # and that you are given the "true" length of the string.
-# (Note: If implementing in Java, please use a charactere array so that you can perform this operation in place.)
+# (Note: If implementing in Java, please use a character array so that you can perform this operation in place.)
 # Example:
-# input: "Mr John Smith     ", 13
-# output: "Mr%20John$20Smith"
+# input:  "Mr John Smith    ", 13
+# output: "Mr%20John%20Smith"
 # Hints: #53, #118
 
+
+# The key here is that it has sufficient space at the end ==> working backwards is no problem.
+def URLify(str, len):
+    print(str)
+    print(len)
+    url_str = []
+    i = int(len) - 1 # conver python string to int
+    print(i)
+    for c in reversed(str):
+        if c == ' ':
+            url_str[i-2] = '%'
+            url_str[i-1] = '2'
+            url_str[i] = '0'
+            i = i - 3
+        else:
+            url_str[i] = c
+    # print(i)
+    return url_str
+
+if __name__ == "__main__":
+    import sys
+    print(URLify(sys.argv[1], sys.argv[2]))
---
+++ CCI/ch10/10.5.sparse_search.py
@@ -0,0 +1,13 @@
+# 10.5 Sparse Search
+#   Given a sorted array of strings that is interspersed with empty strings,
+#   write a method to find the locatoin of a given string.
+#   Example:
+#   input: ball, {"at", "", "", "", "ball", "", "", "car", "", "", "dad", "", ""}
+#   output: 4
+
+def sparseSearch(target, arr):
+
+
+arr = {"at", "", "", "", "ball", "", "", "car", "", "", "dad", "", ""}
+# assert(sparseSearch("ball", arr)) == 4
+sparseSearch("ball", arr)
