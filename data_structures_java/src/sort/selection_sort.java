866997ad807b0cad881009f62734b610ced05b57
Heeyeon Lee <heeyoenl@byu.edu>
2021-01-23 09:16:08 -0700

    selection sort complete

--- data_structures_java/src/sort/selection_sort.java
+++ data_structures_java/src/sort/selection_sort.java
@@ -1,4 +1,48 @@
 package sort;
 
+/*
+    In-place algorithm (no extra memory used)
+    O(n^2) time complexity - quadratic
+    Doesn't require as much swapping as bubble sort
+    Unstable algorithm
+
+    Steps
+        Traverse from the beginning
+        Compare first two and set i to largest element
+        Keep going until the end
+        Change last element to the biggest element of the array
+        Repeat
+ */
 public class selection_sort {
+
+    public static void main(String[] args) {
+        int[] selection = { 20, 35, -15, 7, 55, 1, -22 };
+
+        for (int i = selection.length - 1; i > 0; i--) {
+            int largest = 0; // so far we know the first element is largest. (index 0)
+
+            for (int j = 1; j <= i; j++ ) { // start at index 1, largest is index 0
+                if (selection[j] > selection[largest]) {
+                    largest = j;
+                }
+            }
+            swap(selection, i, largest);
+        }
+
+
+        for (int i: selection) {
+            System.out.println(i);
+        }
+
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        if (i == j) {
+            return;
+        }
+
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
