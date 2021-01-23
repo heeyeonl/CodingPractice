8a8b14486cca464c8a2dc7170cf01f2817e195fb
Heeyeon Lee <heeyoenl@byu.edu>
2021-01-23 08:48:09 -0700

    added selection sort

--- data_structures_java/src/sort/stable_unstable_sort.java
+++ data_structures_java/src/sort/stable_unstable_sort.java
@@ -4,11 +4,12 @@ package sort;
     Unstable - relative ordering of duplicate items is not preserved
     Stable - when it is preserved
 
+    It matters when it's an Object!
+
     ex) 5, 9, 3, 9, 8, 4
      -> 3, 4, 5, 8, 9, 9
                     -  -  if the order of 9 is flipped: unstable
                                             it not flipped: stable
-
     ex) bubble sort is stable sort.
  */
 
