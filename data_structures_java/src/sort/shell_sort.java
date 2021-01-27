e93c80e4aa4ac9d24859d2894c79c31f5ea68339
Heeyeon Lee <heeyoenl@byu.edu>
2021-01-27 09:21:20 -0700

    added shell sort implementation

--- data_structures_java/src/sort/shell_sort.java
+++ data_structures_java/src/sort/shell_sort.java
@@ -7,5 +7,32 @@ public class shell_sort {
         Shell sort starts out using a larger gap value.
         The gap is reduced. (The last gap value is always 1)
 
+        In-place algorithm.
+        Difficult to know Big(O), but worst case O(n^2) but it can perform much better.
+        Doesn't require as much shifting as insertion sort.
+        Unstable algorithm.
      */
+    public static void main(String[] args) {
+       int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
+
+       // loops for gap, outer index to move up, and inner index to compare
+        for (int gap = intArray.length / 2; gap > 0; gap /= 2) {
+
+            for(int i = gap; i < intArray.length; i++) {
+                int j = i;
+                int newElement = intArray[i];
+
+                while (j >= gap && intArray[j - gap] > newElement) { // use newElement instead of intArray[i] because it keeps changing
+                    intArray[i] = intArray[j - gap];
+                    j -= gap;
+                }
+                intArray[j] = newElement;
+            }
+        }
+
+
+        for (int i: intArray) {
+            System.out.println(i);
+        }
+    }
 }
