3ce66fdeee518b7b8e49b7b51a976e21ce591b57
Heeyeon Lee <heeyoenl@byu.edu>
2021-03-23 12:54:59 -0600

    finished FizzBuzz

--- LeetCode/src/main/java/Integers/FizzBuzz_412.java
+++ LeetCode/src/main/java/Integers/FizzBuzz_412.java
@@ -1,5 +1,34 @@
 package Integers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FizzBuzz_412 {
 
+    public static void main(String[] args) {
+        int n = 15;
+        List<String> result = fizzBuzz(n);
+        System.out.println(result);
+    }
+
+    public static List<String> fizzBuzz(int n) {
+        int i = 1;
+        List<String> result = new ArrayList<>();
+        while(i <= n) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                result.add("FizzBuzz");
+            }
+            else if (i % 3 == 0) {
+                result.add("Fizz");
+            }
+            else if (i % 5 == 0) {
+                result.add("Buzz");
+            }
+            else {
+                result.add(Integer.toString(i));
+            }
+            i++;
+        }
+        return result;
+    }
 }
